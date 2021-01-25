09f1440eeed821bdf94d8dae5707f6064f0e9c4a
yyt <13145216024@163.com>
2021-01-25 18:50:33 +0800

    zfb code

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function _App(props) {
 
     useEffect(() => {
         // console.clear();
+        message.loading({ content: '获取code中', duration: 0 })
         const wxAppId = 'wxaf9e884cd4ab31b0';
         const aliAppId = '2021002101634074';
         const local = window.location.href;
@@ -36,7 +37,7 @@ function _App(props) {
             if (code) {
                 setCode(code);
                 window.location.href = `https://shop.integral.haimeiyx.com/integral?code=${code}`
-                console.log(code, 'pay-----code');
+                console.log(code, 'wx---pay-----code');
             }
             if (!code) {
                 window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${wxAppId}&redirect_uri=${local}&response_type=code&scope=snsapi_base&state=${configState}#wechat_redirect`;
@@ -45,7 +46,8 @@ function _App(props) {
         else if (/AlipayClient/.test(window.navigator.userAgent)) { // 支付宝
             if (code) {
                 setCode(code);
-                console.log(code, 'pay-----code');
+                window.location.href = `https://shop.integral.haimeiyx.com/integral?code=${code}`
+                console.log(code, 'zfb----pay-----code');
             }
             if (!code) {
                 window.location.href = `https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?app_id=${aliAppId}&scope=auth_base&state=${configState}&redirect_uri=${encodeURIComponent(local)}`;
@@ -58,7 +60,7 @@ function _App(props) {
 
             message.error('请使用微信或支付宝扫码');
         }
-
+        // message.destroy();
     }, []);
 
 
@@ -69,7 +71,7 @@ function _App(props) {
 
     return (
         <div>
-            code:{getCode}
+
         </div>
     )
 }
